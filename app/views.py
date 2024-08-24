4f8059346707d59803a773eb1ca8a9d16e7a44bf
Your Name <vasavilakka398@gmail.com>
2024-08-24 17:40:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def table4(request):
+    return render(request, 'table4.html')
+def table5(request):
+    return render(request, 'table5.html')
+def test1(request):
+    return render(request, 'test1.html')
---
+++ project3/urls.py
@@ -0,0 +1,26 @@
+"""
+URL configuration for project3 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('table4/', table4, name='table4'),
+    path('table5/', table5, name='table5'),
+    path('test1/', test1, name='test1'),
+]
